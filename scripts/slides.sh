6f4eb66cc0c7cad6318612b88dcbe8be0b3c60d8
Andrew White <andrew.white@rochester.edu>
2018-01-30 07:50:30 -0500

    Added windows command for presenting slides and updated date

--- scripts/slides.sh
+++ scripts/slides.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 cp `pwd`/include/custom.css `dirname $1`
 sudo docker run -p 8888:8888 -it -v `pwd`:/home/jovyan/work jupyter/scipy-notebook jupyter nbconvert --to slides $1 --post serve --ServePostProcessor.port=8888 --ServePostProcessor.ip=0.0.0.0 --template=include/custom_slides.tpl  --ServePostProcessor.reveal_cdn=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.4.1
+
+#windows
+#docker run --rm -it -p 8888:8888 -v C:\Users\white\repos\numerical_stats:/home/jovyan jupyter/scipy-notebook jupyter nbconvert --to slides unit_3\lectures\lecture_1.ipynb --post serve --ServePostProcessor.port=8888 --ServePostProcessor.ip=0.0.0.0 --template=include/custom_slides.tpl  --ServePostProcessor.reveal_cdn=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.4.1
